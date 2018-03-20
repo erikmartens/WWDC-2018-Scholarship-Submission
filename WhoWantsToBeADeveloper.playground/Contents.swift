f0d2d53dde82f3aadb5d7cee6428d2cff1bc6382
erikmartens <erikmartens@me.com>
2018-03-20 21:35:53 +0100

    changed main page description

--- WhoWantsToBeADeveloper.playground/Contents.swift
+++ WhoWantsToBeADeveloper.playground/Contents.swift
@@ -7,11 +7,12 @@ import PlaygroundSupport
  In this playground you will learn about the programming language [Swift](https://developer.apple.com/library/content/documentation/Swift/Conceptual/Swift_Programming_Language/),
  by answering questions based on a variation of the popular _Who Wants to Be a Millionaire_ game format. Whether you are a beginner, intermediary or expert, there is a game mode for you.
  
- My personal journey has taught me that anyone can achieve their goals, when they are taught in a fashion that suits their personal needs.
- With this playful approach to learning Swift anyone who enjoys diving into a new topic via a quiz like fashion will feel right at home.
+ My personal journey has taught me that anyone can achieve their goals, when they are taught in a fashion that suits their personal needs. What's more, breaking the ice is essential. Beginning easy but challenging and increasing the difficulty gradually will trigger the thirst for more, instead of creating frustration.
+ 
+With this playful approach to learning [Swift](https://developer.apple.com/library/content/documentation/Swift/Conceptual/Swift_Programming_Language/) and basics about object-oriented programming, anyone who enjoys diving into a new topic via a quiz like fashion, will feel right at home.
  If you have 3 minutes everyday to answer some questions, you will go far.
  
- > To get started open the live view inside the assistant editor. You can also add your own questions to get a more personalized learning experience.
+ > To get started open the live view inside the assistant editor. You can also add your own questions through the game interface, in order to get a more personalized learning experience. Or you hack the `Question.json` file directly.
  
  __Enjoy and hopefully see you at WWDC 18__ ❤️
  */
