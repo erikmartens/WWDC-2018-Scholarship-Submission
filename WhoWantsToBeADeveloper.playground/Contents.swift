93c57487aef618820c5a81cb2723308563b2aec4
erikmartens <erikmartens@me.com>
2018-03-20 12:15:12 +0100

    inital description

---
+++ WhoWantsToBeADeveloper.playground/Contents.swift
@@ -0,0 +1,16 @@
+import SpriteKit
+import PlaygroundSupport
+
+/*:
+ # Welcome to "Who Wants to Be a Developer"
+ 
+ In this playground you will learn about the programming language [Swift](https://developer.apple.com/library/content/documentation/Swift/Conceptual/Swift_Programming_Language/),
+ by answering questions based on a variation of the popular _Who Wants to Be a Millionaire_ game format. Whether you are a beginner, intermediary or expert, there is a game mode for you.
+ 
+ My personal journey has taught me that anyone can achieve their goals, when they are taught in a fashion that suits their personal needs.
+ With this playful approach to learning Swift anyone who enjoys diving into a new topic via a quiz like fashion will feel right at home.
+ 
+ > To get started open the live view inside the assistant editor. You can also add your own questions to get a more personalized learning experience.
+ 
+ __Enjoy!__ ❤️
+ */
