3272ed0184519a2d4b6eade9672e99013f86a543
erikmartens <erikmartens@me.com>
2018-03-30 20:05:12 +0200

    improved about text

--- WhoWantsToBeADeveloper.playground/Sources/Model/AboutModel.swift
+++ WhoWantsToBeADeveloper.playground/Sources/Model/AboutModel.swift
@@ -2,7 +2,8 @@ import SpriteKit
 
 fileprivate let kTextLines = ["Who Wants to Be a Developer",
                               "Created with 💜 by Erik Maximilian Martens\n",
-                              "This Playground was built to live on past the WWDC 2018 scholarship application process. When developing the game the idea was always to share it with the community after the judging process had been completed. Hopefully this project will accelerate the personal journeys of those who want to shape the world, by providing a platform and code examples for learning. Therefore the complete code will be open sourced via GitHub. The repository will be subject to the MIT license, meaning anyone can use, modify or redistribute the contents of this project in part or its entirety in any way they please.\n",
+                              "Answer as many questions as possible in this arcade style quiz game. Try to set a new highscore and even compete with your friends in local player mode. The fifty-fifty-joker gets reset every 10 rounds.",
+                              "This Playground was built to live on past the WWDC 2018 scholarship application process. Hopefully this project will accelerate the personal journeys of those who want to shape the world, by providing a platform and code examples for learning. Therefore this project will be open sourced via GitHub. The complete code and assets will be subject to the MIT license, meaning anyone can use, modify or redistribute the contents of this project in part or its entirety.\n",
                               "👩‍💻 Hope to meet you at the WWDC 18 👨‍💻"]
 
 fileprivate let kTextAttributes = [
@@ -13,6 +14,10 @@ fileprivate let kTextAttributes = [
     [NSAttributedStringKey.font: UIFont.italicSystemFont(ofSize: 16),
      NSAttributedStringKey.foregroundColor: UIColor.white,
      NSAttributedStringKey.paragraphStyle: NSMutableParagraphStyle(alignment: .center)],
+    
+    [NSAttributedStringKey.font: UIFont.systemFont(ofSize: 12),
+     NSAttributedStringKey.foregroundColor: UIColor.white,
+     NSAttributedStringKey.paragraphStyle: NSMutableParagraphStyle(alignment: .justified)],
     
     [NSAttributedStringKey.font: UIFont.systemFont(ofSize: 12),
      NSAttributedStringKey.foregroundColor: UIColor.white,
